02acc3f2d37c91719d6e0bb32e1b2f07d88c7b7a
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-03-01 14:26:44 +0100

    rechtschreibung

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import Account from "../components/Account/Account";
 
 const Home = () => {
   const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  const month =
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  const year =
+    date.getFullYear() < 10 ? `0${date.getFullYear()}` : date.getFullYear();
 
   return (
     <section className="home">
@@ -15,7 +18,7 @@ const Home = () => {
         {day}.{month}.{year} | {hours}:{minutes}
       </p>
       <h1>Banana Bank</h1>
-      <p>Willkommen in deinem Accout, Mia!</p>
+      <p>Willkommen in deinem Account, Mia!</p>
       <Account />
     </section>
   );
